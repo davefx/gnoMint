7f815c46ee32424390bed02b3f7dbe5875df50eb
davefx <davefx@8ce44e8e-8d12-0410-9598-ae135bd4683b>
2008-10-02 18:06:10 +0000

    Implemented PKCS7 importing. Cannot test, as I haven't found yet any PKCS7 file in the internet

--- src/import.c
+++ src/import.c
@@ -28,88 +28,149 @@
 #include "ca.h"
 #include "ca_file.h"
 
+
+void __import_csr (gnutls_x509_crq_t *crq);
+void __import_cert (gnutls_x509_crt_t *cert);
+
+
+
+void __import_csr (gnutls_x509_crq_t *crq)
+{
+	CaCreationData * creation_data = g_new0(CaCreationData, 1);
+	gchar * pem_csr=NULL;
+	size_t size;
+	gchar * error_msg;
+
+	gchar *aux = NULL;
+	
+	size = 0;
+	gnutls_x509_crq_get_dn_by_oid (*crq, GNUTLS_OID_X520_COMMON_NAME, 0, 0, aux, &size);
+	if (size) {
+		aux = g_new0(gchar, size);
+		gnutls_x509_crq_get_dn_by_oid (*crq, GNUTLS_OID_X520_COMMON_NAME, 0, 0, aux, &size);
+		creation_data->cn = g_strdup (aux);
+		g_free (aux);
+		aux = NULL;
+	}	        
+	
+	size = 0;
+	gnutls_x509_crq_export (*crq, GNUTLS_X509_FMT_PEM, pem_csr, &size)  ; 
+	if (size) {
+		pem_csr = g_new0(gchar, size);
+		gnutls_x509_crq_export (*crq, GNUTLS_X509_FMT_PEM, pem_csr, &size);
+		
+	}
+	
+	error_msg = ca_file_insert_csr (creation_data, NULL, pem_csr);
+	
+	
+	if (error_msg) {
+		gchar *message = g_strdup_printf (_("Couldn't import the certificate request. \n"
+						    "The database returned this error: \n\n'%s'"),
+						  error_msg);
+		ca_error_dialog (message);
+		g_free (message);
+	}
+
+}
+
+void __import_cert (gnutls_x509_crt_t *cert)
+{
+	CertCreationData * creation_data = g_new0(CertCreationData, 1);
+	guchar *serial_str = NULL;
+	UInt160 serial;
+	gchar * pem_cert=NULL;
+	size_t size;
+	gchar * error_msg;
+	gboolean is_ca;
+	guint is_critical;
+	gchar *aux = NULL;
+        
+	// For inserting the cert into the database we must get:
+	// - if the certificate is a CA certificate
+	// - serial
+	// - activation time
+	// - expiration time
+	
+	// Is_CA?
+	is_ca = gnutls_x509_crt_get_ca_status (*cert, &is_critical);
+	if (is_ca == GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE)
+		is_ca = FALSE;
+	
+	// Serial
+	size = 0;
+	gnutls_x509_crt_get_serial (*cert, aux, &size);
+	aux = NULL;
+	if (size) {
+		serial_str = g_new0(guchar, size);
+		gnutls_x509_crt_get_serial (*cert, serial_str, &size);
+		uint160_read (&serial, serial_str, size);
+		g_free (serial_str);
+	}
+        
+	
+	// Activation
+	creation_data->activation = gnutls_x509_crt_get_activation_time (*cert);
+	
+	// Expiration
+	creation_data->expiration = gnutls_x509_crt_get_expiration_time (*cert);
+        
+	
+	// Now we re-export the PEM (as the original PEM can be a list of certs)
+	size = 0;
+	gnutls_x509_crt_export (*cert, GNUTLS_X509_FMT_PEM, aux, &size);
+	if (size) {
+		aux = g_new0(gchar, size);
+		gnutls_x509_crt_export (*cert, GNUTLS_X509_FMT_PEM, aux, &size);
+		pem_cert = g_strdup (aux);
+		g_free (aux);
+		aux = NULL;
+	}
+	
+	error_msg = ca_file_insert_imported_cert (creation_data, is_ca, serial, pem_cert);
+        
+	if (pem_cert)
+		g_free (pem_cert);
+	
+	
+	if (error_msg) {
+		gchar *message = g_strdup_printf (_("Couldn't import the certificate. \n"
+						    "The database returned this error: \n\n'%s'"),
+						  error_msg);
+		ca_error_dialog (message);
+		g_free (message);
+	}	
+}
+
+
+
 gboolean import_csr (guchar *file_contents, gsize file_contents_size) 
 {	
 	gboolean successful_import = FALSE;
 	gnutls_x509_crq_t crq;
 	gnutls_datum_t file_datum;
 
-	gchar *aux = NULL;
 
         file_datum.data = file_contents;
         file_datum.size = file_contents_size;
 
-	// Trying to import a Certificate Signing Request in PEM format
+	// Trying to import a Certificate Signing Request 
 
 	if (gnutls_x509_crq_init (&crq) < 0)
 		return FALSE;
 
-	if (gnutls_x509_crq_import (crq, &file_datum, GNUTLS_X509_FMT_PEM) == 0) {
-		CaCreationData * creation_data = g_new0(CaCreationData, 1);
-		gchar * pem_csr=NULL;
-		size_t size;
-		gchar * error_msg;
+	if (gnutls_x509_crq_import (crq, &file_datum, GNUTLS_X509_FMT_PEM) == 0 ||
+	    gnutls_x509_crq_import (crq, &file_datum, GNUTLS_X509_FMT_DER) == 0) {
 
-		size = 0;
-		gnutls_x509_crq_get_dn_by_oid (crq, GNUTLS_OID_X520_COMMON_NAME, 0, 0, aux, &size);
-		if (size) {
-			aux = g_new0(gchar, size);
-			gnutls_x509_crq_get_dn_by_oid (crq, GNUTLS_OID_X520_COMMON_NAME, 0, 0, aux, &size);
-			creation_data->cn = g_strdup (aux);
-			g_free (aux);
-			aux = NULL;
-		}	        
-
-		pem_csr = (gchar *) file_datum.data; 
-		
-		error_msg = ca_file_insert_csr (creation_data, NULL, pem_csr);
-
-		
-		if (error_msg) {
-			gchar *message = g_strdup_printf (_("Couldn't import the certificate request. \n"
-							    "The database returned this error: \n\n'%s'"),
-							  error_msg);
-			ca_error_dialog (message);
-			g_free (message);
-		}
-		successful_import = TRUE;
-		
+		__import_csr (&crq);
+		successful_import = TRUE;		
 	}
 
-	// Trying to import a Certificate Signing Request in DER format
-
-	if (gnutls_x509_crq_import (crq, &file_datum, GNUTLS_X509_FMT_DER) == 0) {
-		CaCreationData * creation_data = g_new0(CaCreationData, 1);
-		gchar * pem_csr=NULL;
-		size_t size;
-
-		size = 0;
-		gnutls_x509_crq_get_dn_by_oid (crq, GNUTLS_OID_X520_COMMON_NAME, 0, 0, aux, &size);
-		if (size) {
-			aux = g_new0(gchar, size);
-			gnutls_x509_crq_get_dn_by_oid (crq, GNUTLS_OID_X520_COMMON_NAME, 0, 0, aux, &size);
-			creation_data->cn = g_strdup (aux);
-			g_free (aux);
-			aux = NULL;
-		}	        
-
-		size = 0;
-		gnutls_x509_crq_export (crq, GNUTLS_X509_FMT_PEM, pem_csr, &size)  ; 
-		if (size) {
-			pem_csr = g_new0(gchar, size);
-			gnutls_x509_crq_export (crq, GNUTLS_X509_FMT_PEM, pem_csr, &size);
-			
-		}
-
-		ca_file_insert_csr (creation_data, NULL, pem_csr);
-
-		successful_import = TRUE;
-	}
+	gnutls_x509_crq_deinit (crq);
 	
 	return successful_import;
 
 }
-
 
 
 gboolean import_certlist (guchar *file_contents, gsize file_contents_size)
@@ -119,8 +180,6 @@ gboolean import_certlist (guchar *file_contents, gsize file_contents_size)
 	gnutls_x509_crt_t *certs = NULL;
 	gnutls_datum_t file_datum;
         guint num_certs = 0;
-
-	gchar *aux = NULL;
 
 	file_datum.size = file_contents_size;
 	file_datum.data = file_contents;
@@ -141,70 +200,7 @@ gboolean import_certlist (guchar *file_contents, gsize file_contents_size)
                 // certificate
 
                 for (i = num_certs - 1; i>=0; i--) {
-                        CertCreationData * creation_data = g_new0(CertCreationData, 1);
-                        guchar *serial_str = NULL;
-                        UInt160 serial;
-                        gchar * pem_cert=NULL;
-                        size_t size;
-                        gchar * error_msg;
-                        gboolean is_ca;
-                        guint is_critical;
-                        
-                        // For inserting the cert into the database we must get:
-                        // - if the certificate is a CA certificate
-                        // - serial
-                        // - activation time
-                        // - expiration time
-
-                        // Is_CA?
-                        is_ca = gnutls_x509_crt_get_ca_status (certs[i], &is_critical);
-                        if (is_ca == GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE)
-                                is_ca = FALSE;
-
-                        // Serial
-                        size = 0;
-                        gnutls_x509_crt_get_serial (certs[i], aux, &size);
-                        aux = NULL;
-                        if (size) {
-                                serial_str = g_new0(guchar, size);
-                                gnutls_x509_crt_get_serial (certs[i], serial_str, &size);
-                                uint160_read (&serial, serial_str, size);
-                                g_free (serial_str);
-                        }
-                        
-
-                        // Activation
-                        creation_data->activation = gnutls_x509_crt_get_activation_time (certs[i]);
-
-                        // Expiration
-                        creation_data->expiration = gnutls_x509_crt_get_expiration_time (certs[i]);
-                        
-   
-                        // Now we re-export the PEM (as the original PEM can be a list of certs)
-                        size = 0;
-                        gnutls_x509_crt_export (certs[i], GNUTLS_X509_FMT_PEM, aux, &size);
-                        if (size) {
-                                aux = g_new0(gchar, size);
-                                gnutls_x509_crt_export (certs[i], GNUTLS_X509_FMT_PEM, aux, &size);
-                                pem_cert = g_strdup (aux);
-                                g_free (aux);
-                                aux = NULL;
-                        }
-		
-                        error_msg = ca_file_insert_imported_cert (creation_data, is_ca, serial, pem_cert);
-                        
-                        if (pem_cert)
-                                g_free (pem_cert);
-
-		
-                        if (error_msg) {
-                                gchar *message = g_strdup_printf (_("Couldn't import the certificate. \n"
-                                                                    "The database returned this error: \n\n'%s'"),
-                                                                  error_msg);
-                                ca_error_dialog (message);
-                                g_free (message);
-                        }
-
+			__import_cert (&certs[i]);
                 }
                 successful_import = TRUE;
 
@@ -219,73 +215,12 @@ gboolean import_certlist (guchar *file_contents, gsize file_contents_size)
                 return FALSE;
 
 	if (gnutls_x509_crt_import (cert, &file_datum, GNUTLS_X509_FMT_DER) == 0) {
-                CertCreationData * creation_data = g_new0(CertCreationData, 1);
-                guchar *serial_str = NULL;
-                UInt160 serial;
-                gchar * pem_cert=NULL;
-                size_t size;
-                gchar * error_msg;
-                gboolean is_ca;
-                guint is_critical;
-                
-                // For inserting the cert into the database we must get:
-                // - if the certificate is a CA certificate
-                // - serial
-                // - activation time
-                // - expiration time
-                
-                // Is_CA?
-                is_ca = gnutls_x509_crt_get_ca_status (cert, &is_critical);
-                if (is_ca == GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE)
-                        is_ca = FALSE;
-                
-                // Serial
-                size = 0;
-                gnutls_x509_crt_get_serial (cert, aux, &size);
-                aux = NULL;
-                if (size) {
-                        serial_str = g_new0(guchar, size);
-                        gnutls_x509_crt_get_serial (cert, serial_str, &size);
-                        uint160_read (&serial, serial_str, size);
-                        g_free (serial_str);
-                }
-                
-                
-                // Activation
-                creation_data->activation = gnutls_x509_crt_get_activation_time (cert);
-                
-                // Expiration
-                creation_data->expiration = gnutls_x509_crt_get_expiration_time (cert);
-                
-                
-                // Now we export the certificate as PEM (as the original was in DER-format)
-                size = 0;
-                gnutls_x509_crt_export (cert, GNUTLS_X509_FMT_PEM, aux, &size);
-                if (size) {
-                        aux = g_new0(gchar, size);
-                        gnutls_x509_crt_export (cert, GNUTLS_X509_FMT_PEM, aux, &size);
-                        pem_cert = g_strdup (aux);
-                        g_free (aux);
-                        aux = NULL;
-                }
-		
-                error_msg = ca_file_insert_imported_cert (creation_data, is_ca, serial, pem_cert);
-                
-                if (pem_cert)
-                        g_free (pem_cert);
-                
-		
-                if (error_msg) {
-                        gchar *message = g_strdup_printf (_("Couldn't import the certificate. \n"
-                                                            "The database returned this error: \n\n'%s'"),
-                                                          error_msg);
-                        ca_error_dialog (message);
-                        g_free (message);
-                }
+		__import_cert (&cert);
                 
                 successful_import = TRUE;
 	}
-	
+
+	gnutls_x509_crt_deinit (cert);
 
 	return successful_import;
 }
@@ -304,31 +239,12 @@ gboolean import_pkey_wo_passwd (guchar *file_contents, gsize file_contents_size)
 	if (gnutls_x509_privkey_init (&privkey) < 0)
 		return FALSE;
 
-	if (gnutls_x509_privkey_import (privkey, &file_datum, GNUTLS_X509_FMT_PEM) == 0) {
-		gchar * pem_privkey=NULL;
-		gchar * error_msg;
-
-		pem_privkey = (gchar *) file_datum.data; 
-		
-		error_msg = ca_file_import_privkey (pem_privkey);
-		
-		if (error_msg) {
-			gchar *message = g_strdup_printf (_("Couldn't import the given private key. \n"
-							    "%s"),
-							  error_msg);
-			ca_error_dialog (message);
-			g_free (message);
-		}
-		successful_import = TRUE;
-		
-	}
-
 	// Trying to import a Private Key in DER format
 
-	if (gnutls_x509_privkey_import (privkey, &file_datum, GNUTLS_X509_FMT_DER) == 0) {
+	if (gnutls_x509_privkey_import (privkey, &file_datum, GNUTLS_X509_FMT_PEM) == 0 ||
+	    gnutls_x509_privkey_import (privkey, &file_datum, GNUTLS_X509_FMT_DER) == 0) {
 		gchar * pem_privkey=NULL;
 		size_t size;
-
 
 		size = 0;
 		gnutls_x509_privkey_export (privkey, GNUTLS_X509_FMT_PEM, pem_privkey, &size)  ; 
@@ -339,9 +255,12 @@ gboolean import_pkey_wo_passwd (guchar *file_contents, gsize file_contents_size)
 		}
 
 		ca_file_import_privkey (pem_privkey);
+		g_free (pem_privkey);
 
 		successful_import = TRUE;
 	}
+
+	gnutls_x509_privkey_deinit (privkey);
 	
 	return successful_import;
 }
@@ -438,13 +357,67 @@ gboolean import_crl (guchar *file_contents, gsize file_contents_size)
         }        
                 		
         successful_import = TRUE;                        
+	gnutls_x509_crl_deinit (crl);
 	
 	return successful_import;
 }
 
 gboolean import_pkcs7 (guchar *file_contents, gsize file_contents_size)
 {
-        return FALSE;
+	gboolean successful_import = FALSE;
+	gnutls_pkcs7_t pkcs7;
+	gnutls_datum_t file_datum;
+
+        file_datum.data = file_contents;
+        file_datum.size = file_contents_size;
+
+	// Trying to import a Private Key in PEM format
+
+	if (gnutls_pkcs7_init (&pkcs7) < 0)
+		return FALSE;
+
+	// Trying to import a Private Key in DER format
+
+	if (gnutls_pkcs7_import (pkcs7, &file_datum, GNUTLS_X509_FMT_PEM) == 0 ||
+	    gnutls_pkcs7_import (pkcs7, &file_datum, GNUTLS_X509_FMT_DER) == 0) {
+		int i;
+		int certs_no = gnutls_pkcs7_get_crt_count (pkcs7);
+		int crl_no = gnutls_pkcs7_get_crl_count (pkcs7);
+
+		for (i=0; i < certs_no; i++) {
+			guchar *raw_cert = NULL;
+			gsize raw_cert_size = 0;
+
+			gnutls_pkcs7_get_crt_raw (pkcs7, i, raw_cert, &raw_cert_size);
+
+			raw_cert = g_new0(guchar, raw_cert_size);
+			gnutls_pkcs7_get_crt_raw (pkcs7, i, raw_cert, &raw_cert_size);
+
+			import_certlist (raw_cert, raw_cert_size);
+
+			g_free (raw_cert);
+		}
+
+		for (i=0; i < crl_no; i++) {
+			guchar *raw_crl = NULL;
+			gsize raw_crl_size = 0;
+			
+			gnutls_pkcs7_get_crl_raw (pkcs7, i, raw_crl, &raw_crl_size);
+
+			raw_crl = g_new0(guchar, raw_crl_size);
+			gnutls_pkcs7_get_crl_raw (pkcs7, i, raw_crl, &raw_crl_size);
+
+			import_crl (raw_crl, raw_crl_size);
+
+			g_free (raw_crl);
+		}
+
+		successful_import = TRUE;
+	}
+
+	gnutls_pkcs7_deinit (pkcs7);
+	
+	return successful_import;
 }
 
 gboolean import_pkcs12 (guchar *file_contents, gsize file_contents_size)
