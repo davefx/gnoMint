5ac91ffa324b429969451bae1d3c0b470cd19470
davefx <davefx@8ce44e8e-8d12-0410-9598-ae135bd4683b>
2008-10-08 08:43:25 +0000

    Updating files for building Debian packages

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the gnomint package
-#
-0 4	* * *	root	gnomint_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(gnomint):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="gnomint" command="/usr/bin/gnomint"
