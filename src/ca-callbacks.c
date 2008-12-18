46c63bfd7c96fbab5cc73e4eaa98bcab3aa82cb7
davefx <davefx@8ce44e8e-8d12-0410-9598-ae135bd4683b>
2008-12-18 06:37:36 +0000

    Fixing a missing include that made gnoMint to not compile over OpenSUSE.

--- src/ca-callbacks.c
+++ src/ca-callbacks.c
@@ -17,10 +17,11 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-#include <glib.h>
-#include <glib-object.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <glib.h>
+#include <glib-object.h>
 #include <glib/gi18n.h>
 
 #include "ca-callbacks.h"
