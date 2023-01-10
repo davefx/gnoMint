763487f22fdf1d333ff5f08b9c817b782d5445a5
Edward Flick <edward@syncsoft.co>
2023-01-10 03:42:08 -0600

    Removed variable reference to enumerations in header file.

--- src/ca_file.h
+++ src/ca_file.h
@@ -69,16 +69,17 @@ gchar * ca_file_revoke_crt_with_date (guint64 id, time_t date);
 
 GList * ca_file_get_revoked_certs (guint64 ca_id, gchar **error);
 
-enum {CA_FILE_CA_COLUMN_ID=0,
+// CaFileCAColumns
+enum CaFileCAColumns {CA_FILE_CA_COLUMN_ID=0,
       CA_FILE_CA_COLUMN_SERIAL=1,
       CA_FILE_CA_COLUMN_SUBJECT=2,
       CA_FILE_CA_COLUMN_DN=3,
       CA_FILE_CA_COLUMN_PARENT_DN=4,
       CA_FILE_CA_COLUMN_PEM=5,
-      CA_FILE_CA_COLUMN_NUMBER=6}
-        CaFileCAColumns;
+      CA_FILE_CA_COLUMN_NUMBER=6};
 
-enum {CA_FILE_CERT_COLUMN_ID=0,
+// CaFileCertColumns
+enum CaFileCertColumns {CA_FILE_CERT_COLUMN_ID=0,
       CA_FILE_CERT_COLUMN_IS_CA=1,
       CA_FILE_CERT_COLUMN_SERIAL=2,
       CA_FILE_CERT_COLUMN_SUBJECT=3,
@@ -90,16 +91,15 @@ enum {CA_FILE_CERT_COLUMN_ID=0,
       CA_FILE_CERT_COLUMN_DN=9,
       CA_FILE_CERT_COLUMN_PARENT_DN=10,
       CA_FILE_CERT_COLUMN_PARENT_ROUTE=11,
-      CA_FILE_CERT_COLUMN_NUMBER=12}
-        CaFileCertColumns;
+      CA_FILE_CERT_COLUMN_NUMBER=12};
 
-enum {CA_FILE_CSR_COLUMN_ID=0,
+// CaFileCSRColumns
+enum CaFileCSRColumns {CA_FILE_CSR_COLUMN_ID=0,
       CA_FILE_CSR_COLUMN_SUBJECT=1,
       CA_FILE_CSR_COLUMN_PRIVATE_KEY_IN_DB=2,
       CA_FILE_CSR_COLUMN_PEM=3,
       CA_FILE_CSR_COLUMN_PARENT_ID=4,
-      CA_FILE_CSR_COLUMN_NUMBER=5}
-        CaFileCSRColumns;
+      CA_FILE_CSR_COLUMN_NUMBER=5};
 
 
 gboolean ca_file_foreach_ca (CaFileCallbackFunc func, gpointer userdata);
